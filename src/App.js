2782ed0a74b622bca1c6348b1055739c0bf6fb06
Yeison <enmanuelquiterios@gmail.com>
2021-10-28 21:01:16 -0400

    Adding Styled Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Pages:
+
+import Home from "./pages/Home";
+import Pricing from "./pages/PricingPage";
+import SignupPage from "./pages/SignupPage";
+
+// React component App;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={SignupPage} />
+        <Route path="/pricing" exact component={Pricing} />
+      </Switch>
+    </Router>
   );
 }
 
